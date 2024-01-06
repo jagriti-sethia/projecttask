133a4c285e8a362449093893ca5907d819748c8c
jagriti-sethia <jain99jagriti@gmail.com>
2024-01-07 03:46:03 +0530

    update

--- taskproject/src/App.js
+++ taskproject/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Rectangle2 from './components/container2/rectangle2';
+import { Rectangle3 } from './components/container3/rectangle3';
 
 function App() {
   return (
     <div className="App">
       <div sty></div>
     <Rectangle2 />
+    <Rectangle3 />
     </div>
   );
 }
--- taskproject/src/components/container3/rectangle3.js
+++ taskproject/src/components/container3/rectangle3.js
@@ -1,16 +1,37 @@
 import React from 'react'
-
+import './rectanglr3.css';
 export const Rectangle3 = () => {
-  return (
-    <div className='container-fluid'>
-        <div className='rectangle3'>
-            <div className='text'></div>
-            <div className='balasir'>
-                <img src='/images.balasir.png' alt='balasir' />
+    return (
+        <div className='container-fluid'>
+            <div className='rectangle3'>
+                <div className='text'>
+                    <div className="bala-sirs-message">Bala Sir’s Message</div>
+                    <div className="message-from-our">Message from our Inspiration</div>
+                    <div className="sri-balaji-university">
+                        Sri Balaji University, Pune means quality and commitment to the
+                        students’ community. We believe that each and every student has the
+                        potential to be groomed to have a successful corporate career.
+                        Converting the right candidates to the right jobs through right training
+                        and development has always been the goal of our Society. We are deeply
+                        pleased to state that we have achieved the same to a larger extent as
+                        can be seen from the Track-Record of campus placements of our management
+                        institutes.
+                    </div>
+                    <div className="we-are-proud">We are proud to state that, within an existence 
+                    of 15 years Sri Balaji Society is known not only for the best infrastructure but 
+                    also for the best faculties. Sri Balaji Society is already ranked as one amongst 
+                    top Three for ‘visiting faculties’ in India by ‘Business India’ dated 17th Oct. 2010,
+                     which means active involvement of corporate stalwarts in imparting instructions and
+                      practical exposure to students about the industry. </div>
+                      <div className='readmore'><i>Read more</i></div>
+    </div>
+                </div>
+                <div className='balasir'>
+                    <img src='./balasir.png' alt='balasir' />
+                </div>
+
             </div>
 
-        </div>
-
-    </div>
-  )
+        
+    )
 }
