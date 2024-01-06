ab05289c282047d91cc19d6a50cb7fd83e3fbeb1
jagriti-sethia <jain99jagriti@gmail.com>
2024-01-07 03:13:44 +0530

    update

--- taskproject/src/App.js
+++ taskproject/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Rectangle2 from './components/container2/rectangle2';
 
 function App() {
   return (
     <div className="App">
-    
+      <div sty></div>
+    <Rectangle2 />
     </div>
   );
 }
---
+++ taskproject/src/components/container3/rectangle3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Rectangle3 = () => {
+  return (
+    <div className='container-fluid'>
+        <div className='rectangle3'>
+            <div className='text'></div>
+            <div className='balasir'>
+                <img src='/images.balasir.png' alt='balasir' />
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
